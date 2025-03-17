2b608f4a94cce5f04ef2efe9a98ed241b4b91fda
adilshad786 <alidilshad697@gmail.com>
2025-03-17 11:38:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,6 @@
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host="65.19.154.90",  # अपने सर्वर का सही IP डालें
-    port=3306,            # MySQL का सही पोर्ट दें
-    user="dilshadhost_786",
-    password="M#BZ.J5QSqMvh9w",  # अपने डेटाबेस का सही पासवर्ड डालें
-    database="dilshadhost_786"
-)
+
 
 cursor = conn.cursor()
 cursor.execute("SELECT DATABASE();")
